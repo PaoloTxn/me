4053fd1cd4068a3d7bbece78522de238acae2b53
Paolo Tecson <ptxn2001@gmail.com>
2020-06-10 10:37:55 +1000

    exc 1 commenting

--- week2/exercise1.py
+++ week2/exercise1.py
@@ -10,19 +10,19 @@ import platform
 
 # I think this will print "hello! Let's get started" by calling the print function.
 print("hello! Let's get started")  # it printed "hello! Let's get started"
-
+# dictionary of words
 some_words = ['what', 'does', 'this', 'line', 'do', '?']
-
+# prints all words
 for word in some_words:
-    print(word)
-
+    print(word)     #printed the words in a for loop
+# there is no variable x so it wont print anything
 for x in some_words:
-    print(x)
-
-print(some_words)
-
+    print(x)        #added x as a variable to be used in some_words
+# will print some words
+print(some_words)   #printed in bracket format
+# if the length of some_words is greater than 3 itll print
 if len(some_words) > 3:
-    print('some_words contains more than 3 words')
+    print('some_words contains more than 3 words')  # printed
 
 def usefulFunction():
     """
