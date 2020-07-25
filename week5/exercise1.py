7cd6415328300ec4c2638c0f3f224c6af27acd29
Paolo Tecson <ptxn2001@gmail.com>
2020-07-25 21:28:38 +1000

    w5

--- week5/exercise1.py
+++ week5/exercise1.py
@@ -1,4 +1,7 @@
 # -*- coding: UTF-8 -*-
+
+import math
+
 """Refactoring.
 
 This exercise contains a complete and working example, but it's very poorly written.
@@ -19,37 +22,46 @@ you'll need to figure out for yourself what to do.
 # This is a terrible function. The rest of the functions in this file do a
 # much better job of what it's trying to do. Once you've has a little look,
 # move on, and eventually delete this function. (And this comment!)
-def do_bunch_of_bad_things():
-    print("Getting ready to start in 9")
-    print("Getting ready to start in 8")
-    print("Getting ready to start in 7")
-    print("Getting ready to start in 6")
-    print("Getting ready to start in 5")
-    print("Getting ready to start in 4")
-    print("Getting ready to start in 3")
-    print("Getting ready to start in 2")
-    print("Getting ready to start in 1")
-    print("Let's go!")
+# def do_bunch_of_bad_things():
+#     print("Getting ready to start in 9")
+#     print("Getting ready to start in 8")
+#     print("Getting ready to start in 7")
+#     print("Getting ready to start in 6")
+#     print("Getting ready to start in 5")
+#     print("Getting ready to start in 4")
+#     print("Getting ready to start in 3")
+#     print("Getting ready to start in 2")
+#     print("Getting ready to start in 1")
+#     print("Let's go!")
 
-    triangle = {"base": 3, "height": 4}
-    triangle["hypotenuse"] = triangle["base"] ** 2 + triangle["height"] ** 2
-    print("area = " + str((triangle["base"] * triangle["height"]) / 2))
-    print("side lengths are:")
-    print("base: {}".format(triangle["base"]))
-    print("height: {}".format(triangle["height"]))
-    print("hypotenuse: {}".format(triangle["hypotenuse"]))
+#     triangle = {"base": 3, "height": 4}
+#     triangle["hypotenuse"] = triangle["base"] ** 2 + triangle["height"] ** 2
+#     print("area = " + str((triangle["base"] * triangle["height"]) / 2))
+#     print("side lengths are:")
+#     print("base: {}".format(triangle["base"]))
+#     print("height: {}".format(triangle["height"]))
+#     print("hypotenuse: {}".format(triangle["hypotenuse"]))
 
-    another_hyp = 5 ** 2 + 6 ** 2
-    print(another_hyp)
+#     another_hyp = 5 ** 2 + 6 ** 2
+#     print(another_hyp)
 
-    yet_another_hyp = 40 ** 2 + 30 ** 2
-    print(yet_another_hyp)
+
+#     yet_another_hyp = 40 ** 2 + 30 ** 2
+#     print(yet_another_hyp)
 
 
 # return a list of countdown messages, much like in the bad function above.
 # It should say something different in the last message.
 def countdown(message, start, stop, completion_message):
-    pass
+    # i = 0
+    while start != (stop - 1):
+        print(f"{message} {start}")
+        start = start - 1
+    print(f"{completion_message}")
+    
+    
+    
+
 
 
 # TRIANGLES
@@ -62,32 +74,41 @@ def countdown(message, start, stop, completion_message):
 # The stub functions are made for you, and each one is tested, so this should
 # hand hold quite nicely.
 def calculate_hypotenuse(base, height):
-    pass
+    h = base ** 2 + height ** 2
+    hsq = math.sqrt(h)
+    return hsq
 
 
 def calculate_area(base, height):
-    pass
+    a = 0.5 * base * height
+    return a
 
 
 def calculate_perimeter(base, height):
-    pass
+    p = base + height + (math.sqrt(base ** 2 + height ** 2))
+    return p
 
 
 def calculate_aspect(base, height):
-    pass
+    if height > base:
+        return "tall"
+    if base > height:
+        return "wide"
+    if base == height:
+        return "equal"
 
 
 # Make sure you reuse the functions you've already got
 # Don't reinvent the wheel
 def get_triangle_facts(base, height, units="mm"):
     return {
-        "area": None,
-        "perimeter": None,
-        "height": None,
-        "base": None,
-        "hypotenuse": None,
-        "aspect": None,
-        "units": None,
+        "area": calculate_area(base, height),
+        "perimeter": calculate_perimeter(base, height),
+        "height": height,
+        "base": base,
+        "hypotenuse": calculate_hypotenuse(base, height),
+        "aspect": calculate_aspect(base, height),
+        "units": units,
     }
 
 
@@ -138,7 +159,21 @@ def tell_me_about_this_right_triangle(facts_dictionary):
         "This is a {aspect} triangle.\n"
     )
 
+
+    a = calculate_area
+    p = calculate_perimeter
+    h = calculate_hypotenuse
+    aspect = calculate_aspect
+    if facts_dictionary["aspect"] == "tall":
+        diagram = tall.format(**facts_dictionary)
+    elif facts_dictionary["aspect"] == "wide":
+        diagram = wide.format(**facts_dictionary)
+    else:
+        diagram = equal.format(**facts_dictionary)
     facts = pattern.format(**facts_dictionary)
+    return( diagram + "\n" + facts)
+
+    
 
 
 def triangle_master(base, height, return_diagram=False, return_dictionary=False):
@@ -191,5 +226,5 @@ def list_of_words_with_lengths(list_of_lengths):
 
 
 if __name__ == "__main__":
-    do_bunch_of_bad_things()
+    # do_bunch_of_bad_things()
     wordy_pyramid("a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5")
